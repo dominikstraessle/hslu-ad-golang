6b3ce558682e9087d7f948766e9073c2e7fad743
dominik-kubuntu <dominik@straessle.me>
2022-05-02 17:05:48 +0200

    Make generic for all number types

--- quicksort/quick.go
+++ quicksort/quick.go
@@ -3,7 +3,7 @@ package quicksort
 import "math/rand"
 
 type Ordered interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~string
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~string | ~byte
 }
 
 func Quicksort[T Ordered](a []T) []T {
