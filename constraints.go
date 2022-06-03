a8a3a8c670919c5130589a3fcdb4995e7b0cd1ba
dominik-kubuntu <dominik@straessle.me>
2022-06-03 11:14:08 +0200

    Add comparable interface

--- constraints.go
+++ constraints.go
@@ -3,3 +3,11 @@ package ad
 type Ordered interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~string
 }
+
+type AnyComparable interface {
+	Comparable | comparable
+}
+
+type Comparable interface {
+	CompareTo(c any) int
+}
