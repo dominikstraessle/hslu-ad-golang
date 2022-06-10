64a59aacc2c73e6ce7b6c99b60243d36c2593747
dominik-kubuntu <dominik@straessle.me>
2022-06-10 15:44:37 +0200

    Add comments to the prime number generator

--- concurrency/prime/prime.go
+++ concurrency/prime/prime.go
@@ -12,38 +12,58 @@ import (
 	"sync/atomic"
 )
 
+//primesConcurrent generates and prints up to rounds valid prime numbers
 func primesConcurrent(rounds uint32) {
+	// number of workers
 	nWorkers := runtime.NumCPU()
 	ctx := context.Background()
 	pool := worker.New(nWorkers)
+
+	// semaphore to control number of active workers
 	sema := semaphore.NewWeighted(int64(nWorkers))
+	// the maximum possible random number -> used as limit for the random number generator
 	max := getMaxRandomNumber()
+
+	// channel receiving the valid prime numbers
 	results := make(chan *big.Int)
 
+	// the number of validated prime numbers
 	var numberOfPrimes uint32
 
 	go func() {
+		// repeat as long as the wanted number of prime numbers is reached
 		for atomic.LoadUint32(&numberOfPrimes) < rounds {
+			// wait and get until a semaphore is available
 			err := sema.Acquire(ctx, 1)
 			if err != nil {
 				panic(err)
 			}
+
+			// run the prime number finder in the worker pool
 			pool.Execute(func() {
 				randomNumber := getRandomNumber(max)
 				if randomNumber.ProbablyPrime(math.MaxInt) {
+					// send prime number to channel
 					results <- randomNumber
+					// increment the number of valid primes
 					atomic.AddUint32(&numberOfPrimes, 1)
 				}
+
+				// release the resource
 				sema.Release(1)
 			})
 		}
+
+		// number of prime numbers is reached -> close the channel
 		close(results)
 	}()
 
+	// print all prime numbers from the channel
 	for prime := range results {
 		fmt.Printf("%v: %s\n", atomic.LoadUint32(&numberOfPrimes), prime.String())
 	}
 
+	// stop the worker pool
 	pool.Stop()
 }
 
