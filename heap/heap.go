38dd2bf3142132154b8b8fb5f69fece096472aa3
dominik-kubuntu <dominik@straessle.me>
2022-06-14 18:29:52 +0200

    Implement heapsort (some tests still failing)

--- heap/heap.go
+++ heap/heap.go
@@ -1,29 +1,46 @@
 package heap
 
-type IntegerHeap interface {
-	Insert(e byte)
-	DeleteRoot() byte
-	Print() string
+import (
+	"ad"
+)
+
+type IntegerHeap[T ad.Ordered] interface {
+	Insert(e T)
+	DeleteRoot() T
+	Print() []T
+}
+
+func Heapsort[T ad.Ordered](arr []T) []T {
+	h := NewFixedSizeHeap[T](len(arr))
+	for _, t := range arr {
+		h.Insert(t)
+	}
+	for i := len(arr) - 1; i >= 0; i-- {
+		arr[i] = h.DeleteRoot()
+	}
+
+	return arr
 }
 
 // only supports bytes
 // a zero value is used for empty array entries
-type FixedSizeHeap struct {
-	arr       []byte
-	lastIndex int // unused or -1 if full
+type FixedSizeHeap[T ad.Ordered] struct {
+	arr        []T
+	lastIndex  int // unused or -1 if full
+	emptyValue T
 }
 
-func (f *FixedSizeHeap) Print() string {
-	return string(f.arr)
+func (f *FixedSizeHeap[T]) Print() []T {
+	return f.arr
 }
 
-func NewFixedSizeHeap(size int) IntegerHeap {
-	return &FixedSizeHeap{
-		arr: make([]byte, size),
+func NewFixedSizeHeap[T ad.Ordered](size int) IntegerHeap[T] {
+	return &FixedSizeHeap[T]{
+		arr: make([]T, size),
 	}
 }
 
-func (f *FixedSizeHeap) Insert(e byte) {
+func (f *FixedSizeHeap[T]) Insert(e T) {
 	if f.lastIndex == -1 {
 		panic("heap is full!")
 	}
@@ -45,7 +62,7 @@ func (f *FixedSizeHeap) Insert(e byte) {
 	f.switchParent(e, i)
 }
 
-func (f *FixedSizeHeap) switchParent(e byte, i int) {
+func (f *FixedSizeHeap[T]) switchParent(e T, i int) {
 	j := parentIndex(i)
 
 	if f.arr[j] >= e {
@@ -56,7 +73,6 @@ func (f *FixedSizeHeap) switchParent(e byte, i int) {
 
 	f.switchParent(e, j)
 	return
-
 }
 
 func parentIndex(i int) int {
@@ -71,18 +87,18 @@ func rightChildIndex(i int) int {
 	return 2 * (i + 1)
 }
 
-func (f *FixedSizeHeap) DeleteRoot() byte {
+func (f *FixedSizeHeap[T]) DeleteRoot() T {
 	e := f.arr[0]
-	if e == 0 || f.lastIndex == 0 {
+	if e == f.emptyValue || f.lastIndex == 0 {
 		return e
 	}
 
 	if f.lastIndex == -1 {
 		// move root to the last used index and set to 0 (unused)
-		f.arr[len(f.arr)-1], f.arr[0] = 0, f.arr[len(f.arr)-1]
+		f.arr[len(f.arr)-1], f.arr[0] = f.emptyValue, f.arr[len(f.arr)-1]
 	} else {
 		// move root to the last used index and set to 0 (unused)
-		f.arr[f.lastIndex-1], f.arr[0] = 0, f.arr[f.lastIndex-1]
+		f.arr[f.lastIndex-1], f.arr[0] = f.emptyValue, f.arr[f.lastIndex-1]
 	}
 
 	// check and switch children
@@ -97,7 +113,7 @@ func (f *FixedSizeHeap) DeleteRoot() byte {
 	return e
 }
 
-func (f *FixedSizeHeap) switchChild(i int) {
+func (f *FixedSizeHeap[T]) switchChild(i int) {
 	leftChild, rightChild := leftChildIndex(i), rightChildIndex(i)
 
 	if leftChild >= len(f.arr) {
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -2,53 +2,45 @@ package heap
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"sort"
 	"testing"
 )
 
 func TestFixedSizeHeap_Insert(t *testing.T) {
-	f := NewFixedSizeHeap(6)
+	f := NewFixedSizeHeap[byte](6)
 	f.Insert(20)
 	f.Insert(10)
 	f.Insert(5)
 	f.Insert(12)
 	f.Insert(7)
 	f.Insert(50)
-	want := string([]byte{50, 12, 20, 10, 7, 5})
-	if f.Print() != want {
-		t.Errorf("want %s got %s", want, f.Print())
-	}
+	want := []byte{50, 12, 20, 10, 7, 5}
+	assert.Equal(t, want, f.Print())
 
 	f.DeleteRoot()
-	want = string([]byte{20, 12, 5, 10, 7, 0})
-	if f.Print() != want {
-		fmt.Println(f.Print())
-		t.Errorf("want %s got %s", want, f.Print())
-	}
+	want = []byte{20, 12, 5, 10, 7, 0}
+	assert.Equal(t, want, f.Print())
 
 	f.DeleteRoot()
-	want = string([]byte{12, 10, 5, 7, 0, 0})
-	if f.Print() != want {
-		fmt.Println(f.Print())
-		t.Errorf("want %s got %s", want, f.Print())
-	}
+	want = []byte{12, 10, 5, 7, 0, 0}
+	assert.Equal(t, want, f.Print())
 
 	f.DeleteRoot()
-	want = string([]byte{10, 7, 5, 0, 0, 0})
-	if f.Print() != want {
-		fmt.Println(f.Print())
-		t.Errorf("want %s got %s", want, f.Print())
-	}
+	want = []byte{10, 7, 5, 0, 0, 0}
+	assert.Equal(t, want, f.Print())
 }
 
 func TestFixedSizeHeap_InsertFull(t *testing.T) {
 	t.Skip()
-	f := NewFixedSizeHeap(1)
+	f := NewFixedSizeHeap[byte](1)
 	f.Insert(1)
 	f.Insert(1)
 }
 
 func TestFixedSizeHeap_InsertSameSize(t *testing.T) {
-	f := NewFixedSizeHeap(10)
+	f := NewFixedSizeHeap[byte](10)
 	f.Insert('a')
 	f.Insert('a')
 	f.Insert('b')
@@ -60,15 +52,115 @@ func TestFixedSizeHeap_InsertSameSize(t *testing.T) {
 	f.Insert('a')
 	f.Insert('x')
 	fmt.Println(f.Print())
-	if f.Print() != "xxbacaaaaa" {
-		t.Errorf("Expected %s got %s", "xxbacaaaaa", f.Print())
-	}
+	want := []byte("xxbacaaaaa")
+	assert.Equal(t, want, f.Print())
 }
 
 func TestFixedSizeHeap_DeleteEmpty(t *testing.T) {
-	f := NewFixedSizeHeap(10)
+	f := NewFixedSizeHeap[int](10)
 	root := f.DeleteRoot()
 	if root != 0 {
 		t.Errorf("Expected 0 but got %v", root)
 	}
+}
+
+func TestHeapsort(t *testing.T) {
+	x, y := getRandomizedIntSliceAndSortedSolution()
+	type args struct {
+		a []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "inorder",
+			args: args{
+				a: []int{0, 1, 2, 3, 4, 5},
+			},
+			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "reverse",
+			args: args{
+				a: []int{5, 4, 3, 2, 1, 0},
+			},
+			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "sort",
+			args: args{
+				a: []int{2, 3, 1, 4, 5, 0},
+			},
+			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			//TODO: why is the test failing?
+			name: "big slice",
+			args: args{
+				a: x,
+			},
+			want: y,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, Heapsort[int](tt.args.a))
+		})
+	}
+}
+
+func BenchmarkHeapsortBigSlice(b *testing.B) {
+	var x []int
+	for i := 0; i < 100_000; i++ {
+		x = append(x, rand.Intn(1_000_000))
+	}
+	for i := 0; i < b.N; i++ {
+		Heapsort[int](x)
+	}
+}
+
+func BenchmarkHeapsort(b *testing.B) {
+	var x []int
+	for i := 0; i < 10000; i++ {
+		x = append(x, rand.Intn(10000))
+	}
+	for i := 0; i < b.N; i++ {
+		Heapsort[int](x)
+	}
+}
+
+func BenchmarkHeapsortOptimistic(b *testing.B) {
+	var y []int
+	for i := 0; i < 10000; i++ {
+		y = append(y, i)
+	}
+	for i := 0; i < b.N; i++ {
+		Heapsort[int](y)
+	}
+}
+
+func BenchmarkHeapsortPessimistic(b *testing.B) {
+	var z []int
+	for i := 10000; i > 0; i-- {
+		z = append(z, i)
+	}
+	for i := 0; i < b.N; i++ {
+		Heapsort[int](z)
+	}
+}
+
+func getRandomizedIntSliceAndSortedSolution() ([]int, []int) {
+	var x []int
+	size := 10000
+	for i := 0; i < size; i++ {
+		x = append(x, rand.Intn(size))
+	}
+	y := make([]int, size)
+	copy(y, x)
+	sort.Slice(y, func(i, j int) bool {
+		return y[i] < y[j]
+	})
+	return x, y
 }
