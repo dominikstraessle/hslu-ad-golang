54e89af346f0feeba395030babec61289ea564c2
dominik-kubuntu <dominik@straessle.me>
2022-06-14 11:27:58 +0200

    Implement bubble sort and optimize tests and benchmarks of the simple sorting algorithms

--- sort/direct_sort.go
+++ sort/direct_sort.go
@@ -33,3 +33,30 @@ func SelectionSort[T ad.Ordered](arr []T) []T {
 	}
 	return arr
 }
+
+func BubbleSort[T ad.Ordered](arr []T) []T {
+	// repeat bubble sort as many times as the array has elements
+	for j := 0; j < len(arr); j++ {
+
+		// count the number of bubbles for each iteration
+		countBubbles := 0
+
+		// for each iteration, iterate over the whole array (minus the last element)
+		for i := 0; i < len(arr)-1; i++ {
+			// if the element on the left is greater than the next -> switch them
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				countBubbles++
+			}
+		}
+
+		// if there was an iteration without any bubbles -> array is sorted and can be returned
+		// this optimization improves the sort algorithm in best, average and worst case significantly
+		// from 65793316 to 6652 ns per operation
+		// 9931x speedup
+		if countBubbles == 0 {
+			return arr
+		}
+	}
+	return arr
+}
--- sort/direct_sort_test.go
+++ sort/direct_sort_test.go
@@ -3,10 +3,26 @@ package sort
 import (
 	"math/rand"
 	"reflect"
+	"sort"
 	"testing"
 )
 
-func TestDirectInsert(t *testing.T) {
+func getRandomizedIntSliceAndSortedSolution() ([]int, []int) {
+	var x []int
+	size := 10000
+	for i := 0; i < size; i++ {
+		x = append(x, rand.Intn(size))
+	}
+	y := make([]int, size)
+	copy(y, x)
+	sort.Slice(y, func(i, j int) bool {
+		return y[i] < y[j]
+	})
+	return x, y
+}
+
+func TestInsertionSort(t *testing.T) {
+	x, y := getRandomizedIntSliceAndSortedSolution()
 	type args struct {
 		a []int
 	}
@@ -35,6 +51,13 @@ func TestDirectInsert(t *testing.T) {
 				a: []int{2, 3, 1, 4, 5, 0},
 			},
 			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "big slice",
+			args: args{
+				a: x,
+			},
+			want: y,
 		},
 	}
 	for _, tt := range tests {
@@ -47,6 +70,7 @@ func TestDirectInsert(t *testing.T) {
 }
 
 func TestSelectionSort(t *testing.T) {
+	x, y := getRandomizedIntSliceAndSortedSolution()
 	type args struct {
 		a []int
 	}
@@ -76,17 +100,72 @@ func TestSelectionSort(t *testing.T) {
 			},
 			want: []int{0, 1, 2, 3, 4, 5},
 		},
+		{
+			name: "big slice",
+			args: args{
+				a: x,
+			},
+			want: y,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := SelectionSort(tt.args.a); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("InsertionSort() = %v, want %v", got, tt.want)
+				t.Errorf("SelectionSort() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func BenchmarkDirectInsert(b *testing.B) {
+func TestBubbleSort(t *testing.T) {
+	x, y := getRandomizedIntSliceAndSortedSolution()
+	type args struct {
+		a []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "inorder",
+			args: args{
+				a: []int{0, 1, 2, 3, 4, 5},
+			},
+			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "reverse",
+			args: args{
+				a: []int{5, 4, 3, 2, 1, 0},
+			},
+			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "sort",
+			args: args{
+				a: []int{2, 3, 1, 4, 5, 0},
+			},
+			want: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "big slice",
+			args: args{
+				a: x,
+			},
+			want: y,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BubbleSort(tt.args.a); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BubbleSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkInsertionSort(b *testing.B) {
 	var x []int
 	for i := 0; i < 10000; i++ {
 		x = append(x, rand.Intn(10000))
@@ -96,7 +175,7 @@ func BenchmarkDirectInsert(b *testing.B) {
 	}
 }
 
-func BenchmarkDirectInsertOptimistic(b *testing.B) {
+func BenchmarkInsertionSortOptimistic(b *testing.B) {
 	var y []int
 	for i := 0; i < 10000; i++ {
 		y = append(y, i)
@@ -106,7 +185,7 @@ func BenchmarkDirectInsertOptimistic(b *testing.B) {
 	}
 }
 
-func BenchmarkDirectInsertPessimistic(b *testing.B) {
+func BenchmarkInsertionSortPessimistic(b *testing.B) {
 	var z []int
 	for i := 10000; i > 0; i-- {
 		z = append(z, i)
@@ -143,5 +222,35 @@ func BenchmarkSelectionSortPessimistic(b *testing.B) {
 	}
 	for i := 0; i < b.N; i++ {
 		SelectionSort(z)
+	}
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	var x []int
+	for i := 0; i < 10000; i++ {
+		x = append(x, rand.Intn(10000))
+	}
+	for i := 0; i < b.N; i++ {
+		BubbleSort(x)
+	}
+}
+
+func BenchmarkBubbleSortOptimistic(b *testing.B) {
+	var y []int
+	for i := 0; i < 10000; i++ {
+		y = append(y, i)
+	}
+	for i := 0; i < b.N; i++ {
+		BubbleSort(y)
+	}
+}
+
+func BenchmarkBubbleSortPessimistic(b *testing.B) {
+	var z []int
+	for i := 10000; i > 0; i-- {
+		z = append(z, i)
+	}
+	for i := 0; i < b.N; i++ {
+		BubbleSort(z)
 	}
 }
